2592e5bad163fe7862024fcbfdb74ca8577b6616
Dara Dermody <daradermody@gmail.com>
2023-03-24 16:43:37 +0000

    Added link to repo

--- src/AppInfo.tsx
+++ src/AppInfo.tsx
@@ -13,8 +13,9 @@ export function AppInfo({fetchCount, loadTime}: AppInfoProps) {
       <AlertTitle><b>{fetchCount} JS files</b> fetched in <b>{roundedLoadTime} seconds</b> to render this app</AlertTitle>
       Boy, that was slow to load! That's because this simple todo app is built without a bundler. It uses{' '}
       <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules">JavaScript modules</a>{' '}
-      to individually import all sources files <i>and</i> third-party libraries on demand. For each JS file fetched and parsed, the browser {' '}
-      fetches other modules it depends on, and so on, which means a cascade of requests (check out the Network tab in your dev tools!)
+      to individually import all sources files <i>and</i> third-party libraries on demand. For each JS file fetched and parsed, the browser{' '}
+      fetches other modules it depends on, and so on, which means a cascade of requests (check out the Network tab in your dev tools!). See{' '}
+      <a target="_blank" href="https://github.com/daradermody/todo-app-nobundler-nodejs">here</a> for more info.
     </Alert>
   )
 }
